f303967dfe56a9c287e5186a50bf6e0c5c9a3083
Prashanth <42304881+Prashi121@users.noreply.github.com>
2019-01-10 09:00:37 +0530

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 5
+    },
+    "rules": {
+        "no-mixed-spaces-and-tabs":0,
+        "no-undef":0,
+        "no-console":0,
+        "no-unused-vars":0,
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
